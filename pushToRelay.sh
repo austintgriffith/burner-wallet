95ffe199c575029f3d14eaa1e392938ae97ec2c3
Austin <austin.griffith@madwiremedia.com>
2018-11-08 13:03:40 -0700

    sync up

---
+++ pushToRelay.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#move compiled files and production address to relay at:
+#cryptogs box at:
+#/home/ubuntu/burner-wallet/src/contracts
+scp src/contracts/* cryptogs:/home/ubuntu/burner-wallet/src/contracts/
