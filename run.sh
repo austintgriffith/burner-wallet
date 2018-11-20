4e9cbebecaae58deae59402d7ffa0100e19ee391
DigiD <digid@Cybernetic-Node-1A.local>
2018-11-19 21:10:54 -0800

    Setting HTTPS env variable to true for qr-code-reader to work cross platform and mobile

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -ti --rm --name clevis -p 3000:3000 -p 8545:8545 -v ${PWD}:/dapp austingriffith/clevis
+docker run -e "HTTPS=true" -ti --rm --name clevis -p 3000:3000 -p 8545:8545 -v ${PWD}:/dapp austingriffith/clevis
