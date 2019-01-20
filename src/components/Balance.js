3f2b718ef3927337cc11e322b48213abb4e5d7fd
Austin <austin.griffith@madwiremedia.com>
2019-01-20 15:41:34 -0700

    touch up

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -22,7 +22,7 @@ export  default ({icon, text, selected, amount, address, dollarDisplay}) => {
           {text}
         </div>
       </div>
-      <div style={{position:"absolute",right:10,marginTop:15}}>
+      <div style={{position:"absolute",right:25,marginTop:15}}>
         <Scaler config={{startZoomAt:400,origin:"200px 30px",adjustedZoom:1}}>
           <div style={{fontSize:40,letterSpacing:-2}}>
             ${dollarDisplay(amount)}
