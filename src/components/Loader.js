1094719b6b80da3e48b4a8cae9ae8175a5696d25
Austin <austin.griffith@madwiremedia.com>
2019-01-21 13:45:19 -0700

    load

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -16,19 +16,21 @@ class App extends Component {
     clearInterval(interval)
   }
   loadMore(){
-    let newPercent = this.state.percent+4.75
+    let newPercent = this.state.percent+3
     if(newPercent>100) newPercent=100
     this.setState({percent:newPercent})
   }
   render() {
     return (
-      <div>
-        <div style={{position:"relative",width:"70%",margin:'auto',marginTop:-50,opacity:0.1}}>
+      <div style={{textAlign:'center'}}>
+        <img src ={burnerloader} style={{width:"75%",height:"75%",maringTop:"40%",opacity:0.16}}/>
+        <div style={{position:"relative",width:"100%",hegiht:"100%",margin:'auto',marginTop:"-50%",opacity:0.07}}>
           <ReactLoading type="cylon" color={"#FFFFFF"} width={"100%"} />
-          <div style={{position:"absolute",left:0,top:"200%",width:this.state.percent+"%",height:"120%",backgroundColor:"#FFFFFF",opacity:0.3}}>
+          <div style={{position:"absolute",left:0,top:"200%",width:this.state.percent+"%",height:"300%",backgroundColor:"#FFFFFF",opacity:0.3}}>
           </div>
         </div>
-        <img src ={burnerloader} style={{width:"150%",height:"150%",marginLeft:"-25%",opacity:0.50}}/>
+        <div style={{height:400}}>
+        </div>
       </div>
     )
   }
