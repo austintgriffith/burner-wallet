bf7d84dd53785624c5b4098bbcea3bbb171f2658
Ricardo Rius <ricardorius@live.com.mx>
2019-01-31 13:10:46 -0600

    Added Links expiration

--- contracts/Links/Links.sol
+++ contracts/Links/Links.sol
@@ -1,11 +1,17 @@
 pragma solidity 0.4.25;
+/* solium-disable security/no-send */
+/* solium-disable security/no-block-members */
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/utils/Address.sol";
 import "openzeppelin-solidity/contracts/cryptography/ECDSA.sol";
 
+/// @title Send xDai/Eth with a link.
+/// @author Austin Griffith  - <austin@gitcoin.co>
+/// @author Ricardo Rius  - <ricardo@rius.info>
+/// @notice Funds have an adjustable expiration time.
+/// After a fund expires it can only be claimed by the original sender.
 contract Links {
-
     using SafeMath for uint256;
     using Address for address;
     using ECDSA for bytes32;
@@ -15,6 +21,8 @@ contract Links {
         address signer;
         uint256 value;
         uint256 nonce;
+        uint256 creationTime;
+        uint256 expirationTime;
         bool claimed;
     }
     uint256 public  contractNonce = 1;
@@ -36,33 +44,77 @@ contract Links {
         bool claimed
     );
 
+    /// @dev Verifies if it is a valid Id.
+    modifier ifValidId(bytes32 Id){
+        if(isFundValid(Id)){
+            _;
+        } else{
+            revert("Links::ifValidId, Id does not exists.");
+        }
+    }
+    /// @dev Verifies if the Id exists.
+    modifier ifNotValidId(bytes32 Id){
+        if(!isFundValid(Id)){
+            _;
+        } else{
+            revert("Links::ifNotValidId, Id exists.");
+        }
+    }
+    /// @dev Verifies if the claim is valid.
+    modifier ifValidClaim(        
+        bytes32 Id, 
+        bytes memory Signature, 
+        bytes32 ClaimHash, 
+        address Destination
+    ){
+        if(isClaimValid(Id,Signature,ClaimHash,Destination)){
+            _;
+        } else{
+            revert("Links::ifValidClaim, invalid claim.");
+        }
+    }
+    /// @dev Verifies if it is a valid Signature lenght.
+    modifier ifValidSig(bytes memory Signature){
+        if(Signature.length == 65){
+            _;
+        } else{
+            revert("Links::ifValidSig, invalid signature lenght");
+        }
+    }
+
     /// @dev Create fund.
     /// @param _id Fund lookup key value.
     /// @param _signature Sender signature.
     function send(
         bytes32 _id, 
-        bytes memory _signature
+        bytes memory _signature,
+        uint256 _expirationDays
     )   
         public 
-        payable 
+        ifNotValidId(_id)
+        ifValidSig(_signature)
+        payable
         returns (bool)
     {
         require(msg.value >= 1000000000000000,"Links::send, needs to be at least 0.001 xDai to pay relay reward");
-        require(_signature.length == 65,"Links::send, invalid signature lenght");
-        //make sure there is not already a fund here
-        require(!isFundValid(_id),"Links::send, id already exists");
+        require(_expirationDays >= uint256(0),"Links::send, invalid expiration days");
         address signer = ECDSA.recover(_id.toEthSignedMessageHash(),_signature);
-        //recoverSigner returns: address(0) if invalid signature or incorrect version.
         require(signer != address(0),"Links::send, invalid signer");
+        // defaulting to 6 months expiration
+        uint256 expiration = block.timestamp.add(26 weeks);
         uint256 nonce = contractNonce;
         contractNonce = contractNonce.add(uint256(1));
+        if (_expirationDays >= 1){
+            expiration = block.timestamp.add(_expirationDays.mul(1 days));
+        }
         assert(nonce < contractNonce);
-        //create fund
         funds[_id] = Fund({
             sender: msg.sender,
             signer: signer,
             value: msg.value,
             nonce: nonce,
+            creationTime: block.timestamp,
+            expirationTime: expiration,
             claimed: false
         });
         require(isFundValid(_id) && funds[_id].value == msg.value,"Links::send, invalid fund");
@@ -83,11 +135,11 @@ contract Links {
         uint256 _gasReward
     ) 
         public 
+        ifValidId(_id)
         returns (bool)
     {
-        require(isFundValid(_id),"Links::claim, invalid fund");
-        require(_gasReward <= 1000000000000000, "Links::claim, cannot reward more than 0.001 xDai");
         require(_gasReward <= funds[_id].value,"Links::claim, gas reward is greater than the fund value");
+        require(_gasReward <= 1000000000000000, "Links::claim, cannot reward more than 0.001 xDai");
         return executeClaim(_id,_signature,_claimHash,_destination,_gasReward);
     }
   
@@ -138,23 +190,37 @@ contract Links {
     {
         address sender = funds[_id].sender;
         address signer = funds[_id].signer;
-        uint256 value = funds[_id].value;
         uint256 nonce = funds[_id].nonce;
         /* solium-disable-next-line security/no-inline-assembly */
         assembly {
           // Cannot assume empty initial values without initializating them. 
           sender := and(sender, 0xffffffff)
           signer := and(signer, 0xffffffff)
-          value := and(value, 0xffffffff)
           nonce := and(nonce, 0xffffffff)
         }
         return (
-          sender != address(0) && 
-          signer != address(0) && 
-          value > uint256(0) && 
-          nonce > uint256(0) &&
-          nonce < contractNonce
+          (sender != address(0)) && (signer != address(0)) && (nonce > uint256(0)) && (nonce < contractNonce)
         );
+    }
+
+    /// @dev Validate fund status. 
+    /// @param _id Lookup key id.
+    /// @param _destination Destination address.
+    function isClaimExpired(
+        bytes32 _id, 
+        bytes memory _signature, 
+        bytes32 _claimHash, 
+        address _destination
+    ) 
+        public 
+        view 
+        returns (bool)
+    {
+        if(isClaimValid(_id,_signature,_claimHash,_destination)){
+            return (funds[_id].expirationTime < block.timestamp);
+        }else{
+            return true;
+        }
     }
 
     /// @dev Claim fund value.
@@ -168,41 +234,47 @@ contract Links {
         uint256 _gasReward
     ) 
         private 
+        ifValidClaim(_id,_signature,_claimHash,_destination)
         returns (bool)
     {
-        //makes sure signature is correct and fund is valid.
-        require(isClaimValid(_id,_signature,_claimHash,_destination),"Links::executeClaim, claim is not valid");
         bool status = false;
         uint256 residual = uint256(0);
         bool claimed = funds[_id].claimed;
         uint256 value = funds[_id].value;
         uint256 nonce = funds[_id].nonce;
- 
+        address sender = funds[_id].sender;
+
         assert(nonce < contractNonce);
+        // validate mutex/flag status
         if(claimed == false){
             // !isContract - Preventive measure against deployed contracts. 
             require(!msg.sender.isContract(),"Links::executeClaim, sender should not be a contract");
-            // temporary fund invalidation
+            // mutex activation
             funds[_id].claimed = true;
-            residual = value.sub(_gasReward);
-            // address.send() restricts to 2300 gas units
-            /* solium-disable-next-line security/no-send */
-            status = _destination.send(residual);
-            // update fund with correct status
-            funds[_id].claimed = status;
-            // update fund
-            if(status == true){
-                // DESTROY object so it can't be claimed again and free storage space.
+            // expired funds can only be claimed back by original sender.
+            if(isClaimExpired(_id,_signature,_claimHash,_destination)){
+                require(msg.sender == sender,"Links::executeClaim, not original sender");
+                msg.sender.transfer(value);
                 delete funds[_id];
-            } else{
-                funds[_id].value = residual;
+                status = true;
+            }else{
+                residual = value.sub(_gasReward);
+                // address.send() restricts to 2300 gas units
+                status = _destination.send(residual);
+                // update mutex with correct status
+                funds[_id].claimed = status;
+                // update fund
+                if(status == true){
+                    delete funds[_id];
+                } else{
+                    funds[_id].value = residual;
+                }
+                require(msg.sender.send(_gasReward),"Links::executeClaim, could not pay sender");
             }
-            /* solium-disable-next-line security/no-send */
-            require(msg.sender.send(_gasReward),"Links::executeClaim, could not pay sender");
         } else{
-            status = claimed;
             // DESTROY object so it can't be claimed again and free storage space.
             delete funds[_id];
+            status = true;
         }
         // send out events for frontend parsing
         emit Claimed(_id,msg.sender,value,_destination,nonce,status);
