aad5f1edaa901e3d15442df5cb636723a25008ed
Austin <austin.griffith@madwiremedia.com>
2019-02-05 15:39:25 -0700

    clean up advanced page

--- src/components/Advanced.js
+++ src/components/Advanced.js
@@ -72,14 +72,19 @@ export default class Advanced extends React.Component {
         <div className="col-6 p-1">
           <button className="btn btn-large w-100" style={this.props.buttonStyle.primary}
                   onClick={()=>{
-                    //let pkutils = require("ethereum-mnemonic-privatekey-utils")
-                    //const newPrivateKey = pkutils.getPrivateKeyFromMnemonic(newPrivateKey)
-                    changeView('main')
-                    let possibleNewPrivateKey = this.state.newPrivateKey
-                    if(possibleNewPrivateKey.indexOf("0x")!=0){
-                      possibleNewPrivateKey = "0x"+possibleNewPrivateKey
+                    console.log(this.state.newPrivateKey)
+                    if(this.state && this.state.newPrivateKey && this.state.newPrivateKey.length>=64&&this.state.newPrivateKey.length<=66){
+                      //let pkutils = require("ethereum-mnemonic-privatekey-utils")
+                      //const newPrivateKey = pkutils.getPrivateKeyFromMnemonic(newPrivateKey)
+                      changeView('main')
+                      let possibleNewPrivateKey = this.state.newPrivateKey
+                      if(possibleNewPrivateKey.indexOf("0x")!=0){
+                        possibleNewPrivateKey = "0x"+possibleNewPrivateKey
+                      }
+                      setPossibleNewPrivateKey(possibleNewPrivateKey)
+                    }else{
+                      changeAlert({type: 'warning', message: 'Invalid private key.'})
                     }
-                    setPossibleNewPrivateKey(possibleNewPrivateKey)
                   }}>
             <Scaler config={{startZoomAt:400,origin:"50% 50%"}}>
               <i className="fas fa-plus-square"/> {i18n.t('create')}
@@ -106,7 +111,7 @@ export default class Advanced extends React.Component {
     }
 
     let inputSeedRow = (
-      <div className="content ops row">
+      <div className="content ops row" style={{paddingTop:10}}>
         <div className={inputSeedSize}>
         <input type={this.state.seedPhraseHidden?"password":"text"}  autocorrect="off" autocapitalize="none" className="form-control" placeholder="seed phrase" value={this.state.newSeedPhrase}
                onChange={event => this.setState({newSeedPhrase:event.target.value})} />
@@ -115,10 +120,14 @@ export default class Advanced extends React.Component {
         <div className="col-6 p-1">
           <button className="btn btn-large w-100" style={this.props.buttonStyle.primary}
                   onClick={()=>{
-                    let pkutils = require("ethereum-mnemonic-privatekey-utils")
-                    const newPrivateKey = pkutils.getPrivateKeyFromMnemonic(this.state.newSeedPhrase)
-                    changeView('main')
-                    setPossibleNewPrivateKey("0x"+newPrivateKey)
+                    if(!this.state.newSeedPhrase){
+                      changeAlert({type: 'warning', message: 'Invalid seed phrase.'})
+                    }else{
+                      let pkutils = require("ethereum-mnemonic-privatekey-utils")
+                      const newPrivateKey = pkutils.getPrivateKeyFromMnemonic(this.state.newSeedPhrase)
+                      changeView('main')
+                      setPossibleNewPrivateKey("0x"+newPrivateKey)
+                    }
                   }}>
             <Scaler config={{startZoomAt:400,origin:"50% 50%"}}>
               <i className="fas fa-plus-square"/> {i18n.t('create')}
@@ -132,6 +141,7 @@ export default class Advanced extends React.Component {
       <div style={{marginTop:20}}>
 
       <div>
+        <div style={{width:"100%",textAlign:"center"}}><h5>Learn More</h5></div>
         <div className="content ops row" style={{marginBottom:10}}>
           <div className="col-6 p-1">
             <a href="https://github.com/austintgriffith/burner-wallet" style={{color:"#FFFFFF"}} target="_blank">
@@ -154,25 +164,13 @@ export default class Advanced extends React.Component {
         </div>
       </div>
 
-        {privateKey &&
-        <div>
-          <div className="content ops row" >
-            <div className="col-12 p-1">
-              <button className="btn btn-large w-100" style={this.props.buttonStyle.primary}
-                      onClick={()=>{
-                        console.log("BALANCE",balance)
-                        changeView('burn-wallet')
-                      }}>
-                <Scaler config={{startZoomAt:400,origin:"50% 50%"}}>
-                  <i className="fas fa-fire"/> {i18n.t('burn')}
-                </Scaler>
-              </button>
-            </div>
-          </div>
-        </div>}
+      <hr style={{paddingTop:20}}/>
+
+
 
         {privateKey &&
         <div>
+                    <div style={{width:"100%",textAlign:"center"}}><h5>Private Key</h5></div>
           <div className="content ops row" style={{marginBottom:10}}>
 
             <div className="col-6 p-1">
@@ -200,12 +198,37 @@ export default class Advanced extends React.Component {
           <div className="content ops row">
             {privateKeyQrDisplay}
           </div>
+
         </div>
         }
+
+        {privateKey &&
+        <div>
+          <div className="content ops row" >
+            <div className="col-12 p-1">
+              <button className="btn btn-large w-100" style={this.props.buttonStyle.primary}
+                      onClick={()=>{
+                        console.log("BALANCE",balance)
+                        changeView('burn-wallet')
+                      }}>
+                <Scaler config={{startZoomAt:400,origin:"50% 50%"}}>
+                  <i className="fas fa-fire"/> {i18n.t('burn')}
+                </Scaler>
+              </button>
+            </div>
+          </div>
+          <hr style={{paddingTop:20}}/>
+        </div>}
+
+
+        <div style={{width:"100%",textAlign:"center"}}><h5>Create Account</h5></div>
 
         {inputPrivateKeyRow}
 
         {inputSeedRow}
+
+        <hr style={{paddingTop:20}}/>
+        <div style={{width:"100%",textAlign:"center"}}><h5>Extra Tools</h5></div>
 
         <div className="content ops row">
           <div className="col-6 p-1">
