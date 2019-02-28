1607b658a2683291e4aabc2ed124030baacf7370
Mike Lock <michael.lockwitz@consensys.net>
2019-02-28 14:10:09 -0700

    Updating balance layout.

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Blockies from 'react-blockies';
 import { Scaler } from "dapparatus";
+import { Flex, Text } from "rimble-ui";
 
 export  default ({icon, text, selected, amount, address, dollarDisplay}) => {
 
@@ -32,20 +33,18 @@ export  default ({icon, text, selected, amount, address, dollarDisplay}) => {
 
 
   return (
-    <div className="balance row" style={{opacity,paddingBottom:0,paddingLeft:20}}>
+    <Flex>
       <div className="avatar col p-0">
         {iconDisplay}
         <div style={{position:'absolute',left:60,top:12,fontSize:14,opacity:0.77}}>
           {text}
         </div>
       </div>
-      <div style={{position:"absolute",right:25,marginTop:15}}>
-        <Scaler config={{startZoomAt:400,origin:"200px 30px",adjustedZoom:1}}>
-          <div style={{fontSize:40,letterSpacing:-2}}>
-            {dollarDisplay(amount)}
-          </div>
-        </Scaler>
+      <div>
+        <Text>
+          ${dollarDisplay(amount)}
+        </Text>
       </div>
-    </div>
+    </Flex>
   )
 };
