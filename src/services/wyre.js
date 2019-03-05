4110e5b0770f1d2b05e4b5e8b266896d1646a177
rob shaw <rob@borwahs.com>
2019-03-05 04:40:01 -0500

    Build wrapper around Wyre widget for use in project.

---
+++ src/services/wyre.js
@@ -0,0 +1,49 @@
+/** Third Party **/
+const Wyre = window.Wyre;
+
+/** Constants **/
+const DEVICE_TOKEN__KEY = 'WYRE__DEVICE_TOKEN';
+
+class WyreWrapper {
+    async configure() {
+        let deviceToken = localStorage.getItem(DEVICE_TOKEN__KEY);
+        if (!deviceToken) {
+            let array = new Uint8Array(25);
+            window.crypto.getRandomValues(array);
+            deviceToken = Array.prototype.map.call(array, x => ('00' + x.toString(16)).slice(-2)).join('');
+            localStorage.setItem(DEVICE_TOKEN__KEY, deviceToken);
+        }
+    }
+
+    displayWidget(
+        destinationAddress,
+        destinationAmount,
+        onError = () => {},
+        onComplete = () => {}
+    ) {
+        const deviceToken = localStorage.getItem(DEVICE_TOKEN__KEY);
+
+        const widget = new Wyre.Widget({
+            env: 'prod',
+            accountId: 'AC-396YDETRHDJ',
+            auth: {
+                type: 'secretkey',
+                secretKey: deviceToken
+            },
+            operation: {
+                type: 'debitcard',
+                destCurrency: 'eth',
+                dest: `${destinationAddress}`,
+                destAmount: destinationAmount,
+            }
+        });
+
+        widget.on('complete', onComplete);
+        widget.on('error', onError);
+        widget.on('ready', () => {
+            widget.open();
+        });
+    }
+}
+
+export default new WyreWrapper();
