24359e353a6cc3bc25c9ec6c2803526dbf867eae
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2019-04-10 17:27:43 +0200

    Deploy updated contract

--- src/contracts/ERC721Full.blocknumber.js
+++ src/contracts/ERC721Full.blocknumber.js
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 module.exports = "4185372"
 =======
 module.exports = "4178884"
 >>>>>>> Add ERC721Full
+=======
+module.exports = "4185372"
+>>>>>>> Deploy updated contract
