d43f93d01fe311e9d3e01f9d090bc5240eab89bf
Johann Barbie <johannbarbie@me.com>
2019-04-13 16:17:48 +0200

    add sunDAI

--- src/App.js
+++ src/App.js
@@ -509,7 +509,14 @@ class App extends Component {
     }catch(e){
       console.log("ERROR LOADING DAI Stablecoin Contract",e)
     }
-    this.setState({mainnetweb3,ensContract,daiContract,bridgeContract})
+    let xdaiweb3 = helpers.extendWeb3(new Web3(new Web3.providers.WebsocketProvider(XDAI_PROVIDER)));
+    let pdaiContract
+    try{
+      pdaiContract = new xdaiweb3.eth.Contract(require("./contracts/StableCoin.abi.js"),"0xefb369e2c694bc0ba31945e0d3ac91ab8e943be3")
+    }catch(e){
+      console.log("ERROR LOADING DAI Stablecoin Contract",e)
+    }
+    this.setState({mainnetweb3,ensContract,xdaiweb3,daiContract, pdaiContract, bridgeContract})
   }
   componentWillUnmount() {
     clearInterval(interval)
