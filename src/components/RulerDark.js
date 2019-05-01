e58a97c61acad94a7a20c079efa28040c4f64377
Austin <austin.griffith@madwiremedia.com>
2019-05-01 13:55:27 -0600

    multiple markets

---
+++ src/components/RulerDark.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RulerDark = () => {
+  return (
+    <hr style={{color:"#8F8F8F",opacity:0.9,marginTop:10,marginBottom:10,width:"100%" }}/>
+  )
+};
+
+export default RulerDark;
