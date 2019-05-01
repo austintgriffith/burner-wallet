7314eb435f5a4a508b65d81542e62abf92a48f7f
Edwin Cheung <eccheung4@gmail.com>
2019-04-30 17:45:25 -0700

    ScavengerHunt contract

---
+++ contracts/ScavengerHunt/arguments.js
@@ -0,0 +1,14 @@
+/*
+  Example of passing in a string to the constructor:
+  module.exports = ["hello world"]
+*/
+
+module.exports = [
+  ["0x0160b817631968284783c3f9263fc132fcc23fdf43dcc27d905008ab8b29c911",
+  "0xb5e26d81e563bba9f60bfc6490de654d638b2a2e5b8a2879262ffa37982d2a01",
+  "0xece3496425af2abc40d388acd96f7222e002ed94c1d795dddf45f96c985682a9",
+  "0x73a280069bbf1298843afe69d81ddcdd5009a6116572e5ba0dfeeaa999d1b95d",
+  "0x4b1319e40357667bc2ec6c0fd6d8cf471220fb864cc46fa7be92dff936dcdcb7"],
+  60 * 5,
+  60 * 5 
+]
