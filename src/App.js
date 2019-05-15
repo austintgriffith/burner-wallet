d1766d0aa26b9c95d36934de4ec305b0b28c46f6
Austin <austin.griffith@madwiremedia.com>
2019-05-15 18:15:52 -0400

    have burner reconnect on failure to infura

--- src/App.js
+++ src/App.js
@@ -429,6 +429,9 @@ class App extends Component {
     intervalLong = setInterval(this.longPoll.bind(this),45000)
     setTimeout(this.longPoll.bind(this),150)
 
+    this.connectToRPC()
+  }
+  connectToRPC(){
     let mainnetweb3 = new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/ws/v3/e0ea6e73570246bbb3d4bd042c4b5dac'))
     let ensContract = new mainnetweb3.eth.Contract(require("./contracts/ENS.abi.js"),require("./contracts/ENS.address.js"))
     let daiContract
@@ -550,6 +553,7 @@ class App extends Component {
           }
         }catch(e){
           console.log(e)
+          this.connectToRPC()
         }
 
 
