ca493aafb4f3087c3262a55a3bafcdf4c030bd41
Alex Lunyov <isuntc@gmail.com>
2019-05-27 18:27:25 +0800

    Dedupe web3's bn.js (190 KB gzipped)
    Because of web3's monorepo architecture webpack can't handle it well and duplicates bn.js 10 times or so

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -265,6 +265,7 @@ module.exports = function(webpackEnv) {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
+        'bn.js': require.resolve('bn.js')
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
