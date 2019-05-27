28965288e62948d88117976ca6cdf0155a27b6cc
Alex Lunyov <isuntc@gmail.com>
2019-05-27 18:25:00 +0800

    Replace url-loader with file-loader for images
    It will reduce main bundle by ≈30 KB gzipped and will be better for caching — images won't be included into the bundle and loaded again each time when the code was changed.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -318,9 +318,8 @@ module.exports = function(webpackEnv) {
             // A missing `test` is equivalent to a match.
             {
               test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-              loader: require.resolve('url-loader'),
+              loader: require.resolve('file-loader'),
               options: {
-                limit: 10000,
                 name: 'static/media/[name].[hash:8].[ext]',
               },
             },
