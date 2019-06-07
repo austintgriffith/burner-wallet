65aa69787289111ac740e42b280a20c3059fa3f1
Maksim Daunarovich <max.daunarovich@gmail.com>
2019-06-07 23:18:28 +0300

    Remove border radius on mobile devices

--- src/components/StyledCard.js
+++ src/components/StyledCard.js
@@ -6,6 +6,9 @@ const StyledCard = styled(Card).attrs(()=>({
   p: 3
 }))`
   border-radius: 4px;
+  @media screen and (max-width: 480px){
+    border-radius: 0;
+  }
 `;
 
 export default StyledCard;
