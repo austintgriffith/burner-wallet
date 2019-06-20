c087e5f3b76e32e57751cc5109b668ae74075923
David Mihal <david@dmihal.com>
2019-06-21 00:55:35 +0100

    Add local mode

--- src/core.js
+++ src/core.js
@@ -1,33 +1,31 @@
 import BurnerCore from '@burner-wallet/core';
-import { InjectedSigner, LocalSigner } from '@burner-wallet/core/src/signers';
-import { InfuraGateway, InjectedGateway, XDaiGateway } from '@burner-wallet/core/src/gateways';
+import { InjectedSigner, LocalSigner } from '@burner-wallet/core/signers';
+import { InfuraGateway, InjectedGateway, XDaiGateway, HTTPGateway } from '@burner-wallet/core/gateways';
 import { eth, dai, xdai, ERC20Asset } from '@burner-wallet/assets';
 import localTokenAddress from './contracts/ERC20Vendable.address.js';
 
 // TODO: Move all keys to env variable
 const infuraKey = 'e0ea6e73570246bbb3d4bd042c4b5dac';
 
-const injectedGateway = new InjectedGateway();
-const isGanache = injectedGateway.isAvailable() && injectedGateway.getNetworks()[0] === '5777';
+const signers = [new InjectedSigner(), new LocalSigner()];
+const gateways = [new InjectedGateway(), new InfuraGateway(infuraKey)];
+export let mainAsset = xdai;
 
-const localToken = new ERC20Asset({
-  id: 'testxdai',
-  name: 'Test xDai',
-  network: '5777',
-  address: localTokenAddress,
-  usdPrice: 1,
-});
+if (process.env.REACT_APP_MODE === 'local') {
+  mainAsset = new ERC20Asset({
+    id: 'testxdai',
+    name: 'Test xDai',
+    network: '5777',
+    address: localTokenAddress,
+    usdPrice: 1,
+  });
+  gateways.push(new HTTPGateway('http://localhost:8545', '5777'));
+} else {
+  gateways.push(new XDaiGateway());
+}
 
-export const mainAsset = isGanache ? localToken : xdai;
+const assets = [mainAsset, dai, eth];
 
-const core = new BurnerCore({
-  signers: [new InjectedSigner(), new LocalSigner()],
-  gateways: [
-    injectedGateway,
-    new InfuraGateway(infuraKey),
-    new XDaiGateway(),
-  ],
-  assets: [mainAsset, dai, eth],
-});
+const core = new BurnerCore({ signers, gateways, assets });
 
 export default core;
